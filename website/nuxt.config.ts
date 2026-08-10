dee8c5fb59d56c2d4ce54f326bd714d2f155b282
Ethan Lee Qi Yang <ingstudiosofficial@gmail.com>
2026-08-10 13:29:49 +0800

    revert: remove nuxt content

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['@nuxt/content', '@nuxt/eslint', '@nuxtjs/seo'],
+	modules: ['@nuxt/eslint', '@nuxtjs/seo'],
 	css: ['@/assets/css/main.css'],
 	ogImage: {
 		zeroRuntime: true
