d68c689e368d56f5f9afc6d7de10e1e7e1236ba8
Ethan Lee Qi Yang <ingstudiosofficial@gmail.com>
2026-08-15 10:55:11 +0800

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,7 +125,7 @@ tasks.jpackage {
     
     appName = "Turtlebrowse"
     vendor = "(ing) Studios"
-    appVersion = "1.4.4"
+    appVersion = "1.4.5"
     copyright = "2026 (ing) Studios and Ethan Lee"
 
     input = layout.buildDirectory.dir("libs")
