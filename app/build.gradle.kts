9021249a617c580fcd132df795c2ca3ed6e9ddd1
Ethan Lee Qi Yang <ingstudiosofficial@gmail.com>
2026-09-22 18:24:36 +0800

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ javafx {
     modules("javafx.controls", "javafx.graphics", "javafx.base", "javafx.swing")
 }
 
-version = "1.8.0"
+version = "1.8.1"
 
 tasks.processResources {
     inputs.property("version", project.version)
--- app/src/main/java/dev/ingstudios/turtlebrowse/windows/MainWindow.java
+++ app/src/main/java/dev/ingstudios/turtlebrowse/windows/MainWindow.java
@@ -103,8 +103,8 @@ public class MainWindow extends JFrame {
 			ColorScheme.fromSeed(Color.web("#BDCF47")));
 	public String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.1.0 Safari/537.36";
 	private final String windowId;
-	private final CefAppManager cefAppManager = CefAppManager.getInstance(this);
-	private final CefApp cefApp = cefAppManager.getCefApp();
+	private final CefAppManager cefAppManager;
+	private final CefApp cefApp;
 	public final ProfileDatabase profileDatabase;
 	public String defaultSearchProvider = SearchURLTemplates.searchTemplates.get("brave");
 	public boolean enableDiscordPresence = false;
@@ -132,6 +132,9 @@ public class MainWindow extends JFrame {
 		browserAppearance = profileDatabase.getAppearance();
 		aiSettings = profileDatabase.getAISettings();
 		newtabSettings = profileDatabase.getNewtabSettings();
+
+		cefAppManager = CefAppManager.getInstance(this);
+		cefApp = cefAppManager.getCefApp();
 
 		windowId = "%s_main_window".formatted(profile.getIdAsString());
 		WindowsManager.getInstance()
